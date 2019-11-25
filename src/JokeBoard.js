e49efe208ce95b012084f225f7926b0938d1e75f
Chris Hesterman <spoiler14@comcast.net>
2019-11-25 15:09:04 -0800

    implemented new jokes button functionality

--- src/JokeBoard.js
+++ src/JokeBoard.js
@@ -3,8 +3,8 @@ import Joke from './Joke';
 import './JokeBoard.css';
 import dad from './dadSmall.png';
 
-let fillJokes;
 
+let fillJokes;
 class JokeBoard extends Component {
     constructor(props) {
         super(props);
@@ -13,7 +13,9 @@ class JokeBoard extends Component {
             jokeMax: 10,
         };
         this.getJoke = this.getJoke.bind(this);
+        this.collectJokes = this.collectJokes.bind(this);
         this.handleVotes = this.handleVotes.bind(this);
+        this.handleClick = this.handleClick.bind(this); 
     }
     
     async getJoke() {
@@ -25,19 +27,7 @@ class JokeBoard extends Component {
         }).then(response => response.json());
     }
 
-    handleVotes(votes, jokeIndex) {
-        const jokeArr = Array.from(this.state.jokes);
-        let replacementInfo = {
-            votes: votes,
-            joke: jokeArr[jokeIndex].joke,
-            id: jokeArr[jokeIndex].id
-        }
-        jokeArr.splice(jokeIndex, 1, replacementInfo);
-        this.setState({ jokes: new Set(jokeArr) });
-    }
-
-    componentDidMount() {
-        
+    collectJokes() {
         fillJokes = setInterval(() => {
             this.getJoke().then(data => {
                 let newJokes = this.state.jokes;
@@ -52,9 +42,28 @@ class JokeBoard extends Component {
         }, 90);
     }
 
+    handleClick() {
+        this.setState({ jokes: new Set() })
+        this.collectJokes();
+    }
+
+    handleVotes(votes, jokeIndex) {
+        const jokeArr = Array.from(this.state.jokes);
+        let replacementInfo = {
+            votes: votes,
+            joke: jokeArr[jokeIndex].joke,
+            id: jokeArr[jokeIndex].id
+        }
+        jokeArr.splice(jokeIndex, 1, replacementInfo);
+        this.setState({ jokes: new Set(jokeArr) });
+    }
+
+    componentDidMount() {
+        this.collectJokes();
+    }
+
     componentDidUpdate() {
         if (this.state.jokes.size >= this.state.jokeMax) {
-            console.log(this.state.jokes)
             clearInterval(fillJokes);
         } 
     }
@@ -81,7 +90,7 @@ class JokeBoard extends Component {
                 <div className='JokeBoard-side'>
                     <h1 className='Title' >Greatest Dad Jokes</h1>
                     <img src={dad} alt='generic dad'/>
-                    <button className='JokeBoard-new'>Get New Jokes</button>
+                    <button className='JokeBoard-new' onClick={this.handleClick}>Get New Jokes</button>
                 </div>
                 <div className='JokeBoard'>
                     {
