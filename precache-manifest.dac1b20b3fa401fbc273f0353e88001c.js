5ac975021b60da834dc19fabceebc903eb4cdac1
Chris Hesterman <spoiler14@comcast.net>
2019-12-10 12:42:52 -0800

    Updates

---
+++ precache-manifest.dac1b20b3fa401fbc273f0353e88001c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57139255da8adde9baf4933bdfa8df2c",
+    "url": "/Dad-Humor/index.html"
+  },
+  {
+    "revision": "eb94c2475935bf2fcec7",
+    "url": "/Dad-Humor/static/css/main.c84e382f.chunk.css"
+  },
+  {
+    "revision": "2d528b8d883638f1932a",
+    "url": "/Dad-Humor/static/js/2.b2474200.chunk.js"
+  },
+  {
+    "revision": "eb94c2475935bf2fcec7",
+    "url": "/Dad-Humor/static/js/main.dfed35c2.chunk.js"
+  },
+  {
+    "revision": "41ef784e0af25c8c2761",
+    "url": "/Dad-Humor/static/js/runtime-main.61854c86.js"
+  },
+  {
+    "revision": "252b4f80d535eb73a09cf8fedb157965",
+    "url": "/Dad-Humor/static/media/dadSmall.252b4f80.png"
+  }
+]);
